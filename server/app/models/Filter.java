e02bfb83ff39c6af36fdbcf2b69c6477bbc3ef53
David Gofman <dgofman@gmail.com>
2020-08-26 18:03:51 -0700

    fixed model

--- server/app/models/Filter.java
+++ server/app/models/Filter.java
@@ -4,27 +4,20 @@ import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import io.ebean.annotation.DbJsonB;
 import io.ebean.annotation.History;
 import io.ebean.annotation.Length;
-import io.ebean.annotation.NotNull;
 
 @Entity
 @History
 @Table(name = "filter")
 public class Filter extends BaseModel {
 	
-	@NotNull
 	@Length(50)
-	public String title; //title
+	public String reference2;
 	
-	@Length(50)
-	public String table; //table
+	@ManyToOne
+	public Note notes; //notes_id
 
-	@DbJsonB
-	@NotNull
-	public String content; //content
-	
 	@ManyToOne
 	public User user; //FK user_id - User::filters
 }
